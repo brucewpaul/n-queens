fa624f1f868acb98227c792bd4afbc83bbe73441
brucewpaul <brucewpaul@gmail.com>
2016-11-09 20:30:50 -0800

    Implement nQueens through 2x2, errors on 3x3

--- src/solvers.js
+++ src/solvers.js
@@ -16,7 +16,8 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+
+  var solution = [];
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -24,10 +25,61 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  // var solutionCount = 0;
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  var solutions = [];
+
+  var board = new Board({'n': n});
+
+  var getSolutions = function( rooksLeft, board ) {  
+    debugger;
+    if ( rooksLeft === 0 ) {
+      solutions.push(board);
+      debugger;
+      return;
+    }
+    // row loop
+    for ( var i = 0; i < n; i++ ) {
+      // get row of board of i
+      var row = board.get(i);
+      // check if row has rook      
+      debugger;
+      if ( row.reduce(function(a, b) { return a + b; }) >= 1 ) {
+        debugger;
+        continue;
+      }
+
+      // iterate through each column of row
+      // column loop
+      for ( var j = 0; j < row.length; j++ ) {
+        debugger;
+        if ( j > 0 ) {
+          row[j - 1] = 0;
+        }
+        // add rook to row/col
+        row[j] = 1;
+        // check if pass
+        if ( board.hasAnyColConflicts() || board.hasAnyRowConflicts() ) {
+          debugger;
+          row[j] = 0;
+        } else {
+          debugger;
+          // pass it to getSolutions
+          var tempBoard = jQuery.extend(true, {}, board);
+
+          getSolutions(rooksLeft - 1, tempBoard);
+        }
+      }
+
+    }
+        
+  };                                          
+
+  getSolutions(n, board);
+
+
+  console.log('Number of solutions for ' + n + ' rooks:', solutions.length);
+  return solutions.length;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
